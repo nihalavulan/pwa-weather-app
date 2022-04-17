d5327d8adb8af14f0cd787c8c745ac2436160a84
Nihal-a <nihalavulan1@gamil.com>
2022-04-18 02:43:08 +0530

    Featch weather function done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { useState } from "react";
 import { fetchWeather } from "./api/fetchWeather";
 import './App.css'
 
 function App() {
+
+  const [query, setQuery] = useState('')
+
+  const search = async (e) => {
+    if(e.key === 'Enter'){
+      const data = await fetchWeather(query)
+      console.log(data);
+    }
+  }
+
   return (
-    <h1>Hello</h1>
+    <div className="main-container">
+      <input 
+      type="text"
+      className="search"
+      placeholder="Search Location"
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+      onKeyPress={search}
+      />
+    </div>
   );
 }
 
